12f574371ec9d593bd6707dcb02ce92d580728b9
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-04-16 14:45:07 +0100

    admin redirects

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,14 +3,22 @@
 <small>By <a href="http://twitter.com/_lukewh">Luke Whitehouse</a>. All rights reserved.</small>
 
 <nav>
-    <li>
-        <a href="/admin">Admin Area</a>
-    </li>
-    <li>
-        <a href="/login">Login</a>
-    </li>
+    @if ( Auth::user() )
+        <li>
+            <a href="/admin">Admin Area</a>
+        </li>
 
-    <li>
-        <a href="/register">Register</a>
-    </li>
+        <li>
+            <a href="/logout">Logout</a>
+        </li>
+    @else
+        <li>
+            <a href="/login">Login</a>
+        </li>
+
+        <li>
+            <a href="/register">Register</a>
+        </li>
+    @endif
+
 </nav>
