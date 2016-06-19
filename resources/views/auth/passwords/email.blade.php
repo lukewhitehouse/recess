4cf8a987c3fe138ab68e0e152886a22d66745b31
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-06-19 23:41:55 +0100

    send email link form formatting

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,33 +1,27 @@
 @extends('app')
 
 @section('content')
+    <main class="section section--large">
+        <div class="container container--small">
+            <h1 class="headline u-align-center">Reset password</h1>
 
-    <h1>Reset Password</h1>
+            <div class="content">
+                <p class="intro u-align-center">Enter your email address below and we will send you a password reset link via email.</p>
+            </div> <!-- .content -->
+        </div> <!-- .container -->
 
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
+        <form role="form" method="POST" action="{{ route('auth.password.email') }}" class="form u-push-top@2">
+            {!! csrf_field() !!}
 
-    <form role="form" method="POST" action="{{ url('/password/email') }}">
-        {!! csrf_field() !!}
+            <label for="email">Email address</label>
 
-        <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email">E-Mail Address</label>
-
-            <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}">
+            <input type="email" class="form-control{{ $errors->has('email') ? ' is-error' : '' }}" name="email" id="email" value="{{ old('email') }}" placeholder="a.skywalker@deathstar.com">
 
             @if ($errors->has('email'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
+                <div class="note note--error">{{ $errors->first('email') }}</div>
             @endif
-        </div>
 
-        <button type="submit" class="btn btn-primary">
-            Send Password Reset Link
-        </button>
-    </form>
-
+            <button type="submit" class="btn btn--primary | u-push-top/2">Send Link</button>
+        </form>
+    </main>
 @endsection
