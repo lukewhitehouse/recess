971e5e24cbc7a34f289676c1e5f79f435ad4e98c
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-06-19 23:38:22 +0100

    setup registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,62 +1,52 @@
 @extends('app')
 
 @section('content')
+    <main class="section section--large">
+        <div class="container container--small">
+            <h1 class="headline u-align-center">Register</h1>
 
-    <h1>Register</h1>
-        <form role="form" method="POST" action="{{ url('/register') }}">
+            <div class="content">
+                <p class="intro u-align-center">Fill the form in below to request registration to Mixd's holiday manager.</p>
+            </div> <!-- .content -->
+        </div> <!-- .container -->
+
+        <form role="form" method="POST" action="{{ route('auth.register') }}" class="form u-push-top@2">
             {!! csrf_field() !!}
 
-            <div class="{{ $errors->has('name') ? ' has-error' : '' }}">
-                <label for="name">Name</label>
+            <label for="name">Name</label>
 
-                <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
+            <input type="text" class="form-control{{ $errors->has('name') ? ' is-error' : '' }}" name="name" id="name" value="{{ old('name') }}" placeholder="Anakin Skywalker">
 
-                @if ($errors->has('name'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('name') }}</strong>
-                    </span>
-                @endif
-            </div>
+            @if ($errors->has('name'))
+                <div class="note note--error">{{ $errors->first('name') }}</div>
+            @endif
 
-            <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label for="email">E-Mail Address</label>
+            <label for="email">Email address</label>
 
-                <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}">
+            <input type="email" class="form-control{{ $errors->has('email') ? ' is-error' : '' }}" name="email" id="email" value="{{ old('email') }}" placeholder="a.skywalker@deathstar.com">
 
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-            </div>
+            @if ($errors->has('email'))
+                <div class="note note--error">{{ $errors->first('email') }}</div>
+            @endif
 
-            <div class="{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label for="password">Password</label>
+            <label for="password">Password</label>
 
-                <input type="password" class="form-control" name="password" id="password">
+            <input type="password" class="form-control{{ $errors->has('password') ? ' is-error' : '' }}" name="password" id="password" placeholder="****************">
 
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
+            @if ($errors->has('password'))
+                <div class="note note--error">{{ $errors->first('password') }}</div>
+            @endif
 
-            <div class="{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                <label for="password_confirmation">Confirm Password</label>
+            <label for="password_confirmation">Confirm password</label>
 
-                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation">
+            <input type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-error' : '' }}" name="password_confirmation" id="password_confirmation" placeholder="****************">
 
-                @if ($errors->has('password_confirmation'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                    </span>
-                @endif
-            </div>
+            @if ($errors->has('password_confirmation'))
+                <div class="note note--error">{{ $errors->first('password_confirmation') }}</div>
+            @endif
 
-            <button type="submit" class="btn btn-primary">
-                Register
-            </button>
+            <button type="submit" class="btn btn--primary | u-push-top/2">Register</button>
         </form>
-
+    </main>
 @endsection
+
