4e167b7cabb08cf16b0c31dc82ed7656eca0e51d
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-06-20 01:34:31 +0100

    basic account template formatting

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,53 +1,64 @@
 @extends('app')
 
 @section('content')
-    <h1>Account area! Congrats {{ $user->name }}, you are logged in!</h1>
+    <main class="section section--large">
+        <div class="container container--small u-push-bottom@2">
+            <h1 class="headline u-align-center">Account area</h1>
 
-    @if($holidays)
-        <h5>Holiday requests</h5>
-        <table>
-            <thead>
-                <th>Start date</th>
-                <th>End date</th>
-                <th>Description</th>
-                <th>Type</th>
-                <th>Status</th>
-                <th></th>
-            </thead>
-            <tbody>
-                @foreach($holidays as $i)
-                    <tr>
-                        <td>
-                            {{ $i->date_start }}
-                        </td>
-                        <td>
-                            {{ $i->date_end }}
-                        </td>
-                        <td>
-                            {{ $i->description }}
-                        </td>
-                        <td>
-                            {{ $i->type }}
-                        </td>
-                        <td>
-                            {{ $i->status }}
-                        </td>
-                        <td>
-                            <a href="#">Edit holiday</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
+            <div class="content">
+                <p class="intro u-align-center">Welcome back to your account area {{ $user->name }}</p>
+            </div> <!-- .content -->
+        </div> <!-- .container -->
 
-    @if($user)
-        <h5>User details</h5>
-        <dl>
-            <dt>Name:</dt>
-            <dd>{{ $user->name }}</dd>
-            <dt>Email:</dt>
-            <dd>{{ $user->email }}</dd>
-        </dl>
-    @endif
-@stop
+        <div class="container">
+            @if($holidays)
+                <h5>Holiday requests</h5>
+                <table>
+                    <thead>
+                        <th>Start date</th>
+                        <th>End date</th>
+                        <th>Description</th>
+                        <th>Type</th>
+                        <th>Status</th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        @foreach($holidays as $i)
+                            <tr>
+                                <td>
+                                    {{ $i->date_start }}
+                                </td>
+                                <td>
+                                    {{ $i->date_end }}
+                                </td>
+                                <td>
+                                    {{ $i->description }}
+                                </td>
+                                <td>
+                                    {{ $i->type }}
+                                </td>
+                                <td>
+                                    {{ $i->status }}
+                                </td>
+                                <td>
+                                    <a href="#">Edit holiday</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+
+            @if($user)
+                <h5>User details</h5>
+                <dl>
+                    <dt>Name:</dt>
+                    <dd>{{ $user->name }}</dd>
+                    <dt>Email:</dt>
+                    <dd>{{ $user->email }}</dd>
+                </dl>
+            @endif
+        </div>
+
+    </main>
+@endsection
